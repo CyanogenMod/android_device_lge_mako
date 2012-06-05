3ed9e3bcd3e1f1a97526d7dc81d69950808dac39
Jean-Baptiste Queru <jbq@google.com>
2012-06-05 15:14:38 -0700

    Use proper copyright notices - those files were written from scratch
    Change-Id: Ie512ba5c4356a9ccb892c6de0e4cc90868cd671f

--- full_mako.mk
+++ full_mako.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2011 The Android Open Source Project
+#
+# Copyright 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 # This is only a placeholder, which should be replaced once there's
 # a proper mako device
