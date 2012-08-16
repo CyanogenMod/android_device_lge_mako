b48553d6b5942c4a8f091fbf19b31a8c530bfc45
Iliyan Malchev <malchev@google.com>
2012-08-16 14:40:17 -0700

    mako: support android.software.live_wallpaper
    Fixes b/6999017
    
    Change-Id: I44339a6d9d9ba99d299a97e2569f14ab57e0d217
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- device.mk
+++ device.mk
@@ -99,8 +99,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
-#	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # NFC packages
 PRODUCT_PACKAGES += \
