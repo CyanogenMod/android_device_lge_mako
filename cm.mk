ffdf8d46a21fd560071764adf9732abe2212f3c5
utkanos <utkanos@gmail.com>
2013-01-26 21:56:57 -0600

    MakoParts (adapted from GNexusParts)
    Patch Set 2: Addition of Color/RGB tuning
    Patch Set 3: Cleanup
    Patch Set 4: Added German translation
    
    Change-Id: I93c03954fe275ed9aa7dcd32c5a4226b383504b6

--- cm.mk
+++ cm.mk
@@ -23,5 +23,8 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.2.1/JOP40D/533553:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.2.1 JOP40D 533553 release-keys"
 
+# Add settings app extension
+PRODUCT_PACKAGES += MakoParts
+
 # Enable Torch
 PRODUCT_PACKAGES += Torch
