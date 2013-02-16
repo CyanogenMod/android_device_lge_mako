6c97d939a5ba8ce90722ec95208ce19ed7cd111e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-02-16 02:51:16 +0000

    Add mako's audio package
    This is labeled as being for mako, so use it
    
    Change-Id: Ic4f0b295ce27d084dfb4b44f3a58dbbe5b1416a1

--- device.mk
+++ device.mk
@@ -249,3 +249,6 @@ PRODUCT_COPY_FILES += \
 	system/extras/bugmailer/send_bug:system/bin/send_bug
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# This is the mako-specific audio package
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
