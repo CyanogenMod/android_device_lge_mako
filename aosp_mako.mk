2cbcbb45f1e4d2b46d5660d9a8ee2a45f377eef1
Jean-Baptiste Queru <jbq@google.com>
2013-03-22 15:53:56 -0700

    Rename AOSP builds to aosp_*
    Change-Id: Ia3f5c3482242f519a515754a14fd3f907134c184

--- aosp_mako.mk
+++ aosp_mako.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2012 The Android Open Source Project
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, device/lge/mako/full_mako.mk)
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mako.mk \
-    $(LOCAL_DIR)/full_mako.mk
+PRODUCT_NAME := aosp_mako
--- full_mako.mk
+++ full_mako.mk
@@ -21,12 +21,12 @@
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := full_mako
 PRODUCT_DEVICE := mako
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := Full JellyBean on Mako
+PRODUCT_MODEL := AOSP on Mako
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := true
 
