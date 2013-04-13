03d80e0623b9e72f52dbbb6ca3ee170e47aa555b
Jeff Johnson <jjohnson@codeaurora.org>
2013-04-12 21:05:04 -0700

    mako: add supplicant config overlay
    Change-Id: Ib419f3f361938897e7c307e4e3f1c286d716d84a
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- Android.mk
+++ Android.mk
@@ -23,6 +23,26 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
+ifneq ($(filter mako occam,$(TARGET_DEVICE)),)
+
 LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := wpa_supplicant_overlay.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := p2p_supplicant_overlay.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+include $(BUILD_PREBUILT)
+
+endif
 
 include $(call first-makefiles-under,$(LOCAL_PATH))
