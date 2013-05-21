62bd8a98ff4718b38c58dc07d92c5603f966c67f
Robert Craig <rpcraig@tycho.ncsc.mil>
2013-05-21 06:37:22 -0400

    Initial SELinux policy.
    Some early policy work for bringing up the board.
    
    Change-Id: I882b2c825edcf1fc59a3059ece21a80598c7a419
    Signed-off-by: rpcraig <rpcraig@tycho.ncsc.mil>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,3 +93,33 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/lge/mako
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 -include vendor/lge/mako/BoardConfigVendor.mk
+
+BOARD_SEPOLICY_DIRS += \
+        device/lge/mako/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+	file_contexts \
+	bd_addr_loader.te \
+	bluetooth.te \
+	bridge.te \
+	camera.te \
+	conn_init.te \
+	device.te \
+	dhcp.te \
+	domain.te \
+	file.te \
+	kickstart.te \
+	init.te \
+	init_shell.te \
+	mediaserver.te \
+	mpdecision.te \
+	netmgrd.te \
+	qmux.te \
+	rild.te \
+	rmt.te \
+	sensors.te \
+	surfaceflinger.te \
+	system.te \
+	thermald.te \
+	ueventd.te \
+	wpa_supplicant.te
