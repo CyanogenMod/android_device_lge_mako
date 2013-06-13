9c69290a1f8d6de2457a6ee124a1b2d0907986c9
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-06-14 00:16:20 +0100

    Fix android.os.cts.BuildVersionTest failure
    Change-Id: Ic04d359be9b74d7f3afadbf09d2c4e0563d87d18

--- cm.mk
+++ cm.mk
@@ -17,7 +17,7 @@ $(call inherit-product, device/lge/mako/full_mako.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
 PRODUCT_NAME := cm_mako
-PRODUCT_BRAND := Google
+PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
