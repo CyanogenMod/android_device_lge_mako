3034a3419a269f84a110674a33822a5ce37d6682
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-08-15 01:00:47 +0100

    display: Switch to the qcom/CAF variant
    Change-Id: I953566f896b251afbbb4f2e8d4ddc4695da9bb6b

--- camera/QCameraParameters.h
+++ camera/QCameraParameters.h
@@ -23,6 +23,7 @@
 
 namespace android {
 
+#ifndef QCOM_HARDWARE
 struct FPSRange{
     int minFPS;
     int maxFPS;
@@ -35,6 +36,8 @@ struct FPSRange{
         maxFPS=max;
     };
 };
+#endif
+
 class QCameraParameters: public CameraParameters
 {
 public:
