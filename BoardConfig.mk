6718f2820179ca41b416bcd72182c3db2ce7f517
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-09-06 23:51:47 +0100

    mako: Switch to unified MSM nexus kernel
    Change-Id: I1370fdbf46d1d9b0f200f4fa7a629d43db392760

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 # Try to build the kernel
+TARGET_KERNEL_SOURCE := kernel/google/msm
 TARGET_KERNEL_CONFIG := mako_defconfig
 
 BOARD_USES_ALSA_AUDIO:= true
