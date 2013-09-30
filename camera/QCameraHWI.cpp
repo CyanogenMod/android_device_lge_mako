0be4d675df0654f1b55f0f516893b8cc6f696f56
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-09-30 18:09:48 +0100

    camera: Fix the flash sync issue in CAF mode
    Fix the sync issue between snapshot and flash
    in CAF mode.
    This is still not an ideal solution, but at least
    it doesn't generate black photos anymore
    
    Change-Id: Iccb84c2a00c8b891a8c57c72aa77d970af00bbed

--- camera/QCameraHWI.cpp
+++ camera/QCameraHWI.cpp
@@ -1635,9 +1635,9 @@ status_t  QCameraHardwareInterface::takePicture()
     bool hdr;
     int  frm_num = 1, rc = 0;
     int  exp[MAX_HDR_EXP_FRAME_NUM];
+    isp3a_af_mode_t afMode = getAutoFocusMode(mParameters);
 
     if(QCAMERA_HAL_RECORDING_STARTED != mPreviewState){
-      isp3a_af_mode_t afMode = getAutoFocusMode(mParameters);
       if (!mFlashCond)
       {
         mFlashCond = getFlashCondition();
@@ -1679,8 +1679,12 @@ status_t  QCameraHardwareInterface::takePicture()
             }
             return ret;
         }
-        /*prepare snapshot, e.g LED*/
-        takePicturePrepareHardware( );
+
+
+        if (afMode != AF_MODE_CAF)
+            /*prepare snapshot, e.g LED*/
+            takePicturePrepareHardware( );
+
         /* There's an issue where we have a glimpse of corrupted data between
            a time we stop a preview and display the postview. It happens because
            when we call stopPreview we deallocate the preview buffers hence overlay
@@ -1691,6 +1695,10 @@ status_t  QCameraHardwareInterface::takePicture()
 
         /* stop preview */
         pausePreviewForSnapshot();
+
+        if (afMode == AF_MODE_CAF)
+            /*prepare snapshot, e.g LED*/
+            takePicturePrepareHardware( );
 
         /* call Snapshot start() :*/
         ret =  mStreamSnap->start();
