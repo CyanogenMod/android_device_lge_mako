4075eed7e1f0f33beb5bec7c156b638f4f319031
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-21 06:36:31 +0000

    Replace AOSP's blob list and update the extractors

--- extract-files.sh
+++ extract-files.sh
@@ -6,7 +6,7 @@ DEVICE=mako
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
-for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g'`; do
+for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$`; do
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
