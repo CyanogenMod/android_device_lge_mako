b851e969937eaee2df4639c7b729e7a914d46e5e
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2014-03-26 20:53:38 +0900

    mako: Add default value for display color control
    depend on following commits
    
    opt/hardware: http://review.cyanogenmod.org/#/c/59752/
    Settings: http://review.cyanogenmod.org/59753
    
    Change-Id: I5573bb6c45f4bc67a040b21203a103e0a883903d

--- cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
+++ cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
@@ -32,6 +32,9 @@ public class DisplayColorCalibration {
     public static int getMinValue()  {
         return 0;
     }
+    public static int getDefValue() {
+        return 192;
+    }
     public static String getCurColors()  {
         return FileUtils.readOneLine(COLOR_FILE);
     }
