30482c71223268d10c14970f75532a780e27c9a3
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2014-06-23 19:22:25 +0900

    mako: Update extract files for ignored proprietary files
    This add missing code exclude proprietary files marked as ignored
    start with '-' when execute extracts-files.sh
    
    Change-Id: If792519dece4550d46ba037d175ab893704eb1de

--- extract-files.sh
+++ extract-files.sh
@@ -24,7 +24,7 @@ fi
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
-for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$`; do
+for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ | grep -v ^-`; do
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
