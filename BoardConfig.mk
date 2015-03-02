b3e131a23ea0a7af8ace2f728b8c13babf76ac48
lion0738 <lion0738@naver.com>
2015-03-02 22:33:23 +0900

    sepolicy: modifications for CmHardwareService
    Change-Id: I7b21b6defce2b09c929ff62279f98010e4724009

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,6 @@ BOARD_SEPOLICY_UNION += \
        rmt.te \
        sensors.te \
        surfaceflinger.te \
-       system_app.te \
        system_server.te \
        tee.te \
        te_macros \
