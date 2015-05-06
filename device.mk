44040e8774bce749dcecb8f4ad5505f0e1de159d
Dan Albert <danalbert@google.com>
2015-05-06 13:18:10 -0700

    Whitelist device for stlport.
    Bug: http://b/15193147
    Change-Id: Icde6cba4947ae17b92a7ddbd61a40f9ace839ed4

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ PRODUCT_PACKAGES += \
         VisualizationWallpapers \
         librs_jni
 
+# http://b/15193147
+# TODO(danalbert): Remove this once stlport is dead and gone.
+PRODUCT_PACKAGES +=  libstlport
+
 PRODUCT_COPY_FILES += \
 	device/lge/mako/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
 	device/lge/mako/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
