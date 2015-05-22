67afe79c5f0502635bee5361b7d615bf14ddd993
xtrymind <xtrymind@gmail.com>
2015-05-22 17:35:29 +0700

    update to LMY47V fingerprint and description
    Change-Id: I74aa7c9aa982f868ec1308a0370ac9af141c3fc0

--- cm.mk
+++ cm.mk
@@ -19,8 +19,8 @@ PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1/LMY47O/1783956:user/release-keys \
-   PRIVATE_BUILD_DESC="occam-user 5.1 LMY47O 1783956 release-keys"
+   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY47V/1836172:user/release-keys \
+   PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY47V 1836172 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
