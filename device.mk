49f570a540673150c267813ace5490d50e0f240c
Elektroschmock <elektroschmock78@googlemail.com>
2015-06-24 20:01:03 +0200

    mako: Don't need to copy FFMPEG config anymore
    Change-Id: I14bb33d35c10fac755e7b33d97be2f3d8077b9c8

--- device.mk
+++ device.mk
@@ -70,7 +70,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
 	device/lge/mako/media_codecs.xml:system/etc/media_codecs.xml
 
 # Prebuilt kl and kcm keymaps
