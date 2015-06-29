71017dedf2c01698c4181feebfa8c3193caa4313
Daniel-Medeiros <daniel.medeiros.deb@gmail.com>
2015-06-29 02:59:44 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,6 @@
 # limitations under the License.
 #
 
-add_lunch_combo aosp_mako-userdebug
+add_lunch_combo ownrom_mako-user
+add_lunch_combo ownrom_mako-userdebug
+add_lunch_combo ownrom_mako-eng
