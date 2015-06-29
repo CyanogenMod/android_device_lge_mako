6b6ae352bc2f6fbd8a05bb07322be180571d8e63
Daniel-Medeiros <daniel.medeiros.deb@gmail.com>
2015-06-29 02:49:33 -0300

    Update and rename cm.mk to ownrom.mk

--- ownrom.mk
+++ ownrom.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common OwnROM stuff.
+$(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ownrom/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := cm_mako
+PRODUCT_NAME := ownrom_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
