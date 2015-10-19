3985ace5eb202efa2cf65641d0d3482cff0da7d2
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-10-19 01:03:50 -0700

    Media codecs profiles from flo
    Change-Id: Ibb5537727206d5379a87e665f900af1a69c8a620

--- device.mk
+++ device.mk
@@ -74,7 +74,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-	device/lge/mako/media_codecs.xml:system/etc/media_codecs.xml
+	device/lge/mako/media_codecs.xml:system/etc/media_codecs.xml \
+	device/lge/mako/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 # Prebuilt kl and kcm keymaps
 PRODUCT_COPY_FILES += \
