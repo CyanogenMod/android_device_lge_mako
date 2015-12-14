597ef82343ba285e0d8929ef9254e2b402c95fac
Felipe Leme <felipeal@google.com>
2015-12-14 14:11:32 -0800

    Migrated dumpstate to C++.
    Bug: 25563823
    Change-Id: I3401360f437277e3a6dd6b8a4658fe89f232070a

--- dumpstate/Android.mk
+++ dumpstate/Android.mk
@@ -17,7 +17,7 @@ include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := frameworks/native/cmds/dumpstate
 
-LOCAL_SRC_FILES := dumpstate.c
+LOCAL_SRC_FILES := dumpstate.cpp
 
 LOCAL_MODULE := libdumpstate.mako
 
