7d77e7b20e6a124bb9ae2982df1e4ca1bfcda124
lion0738 <lion0738@naver.com>
2016-01-07 16:40:38 +0900

    mako: use Snap camera app
    fixed sepolicy denial
    
    Change-Id: I9a34b479c452220a75caab54a89a08db0af7c86a

--- device.mk
+++ device.mk
@@ -276,6 +276,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-filter=speed \
     dalvik.vm.dex2oat-swap=false
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
